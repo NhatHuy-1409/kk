4257a9bc660d9bfa6df7f4fd41def999c634a540
nhathuy <nhathuy97ars@gmail.com>
2022-10-12 16:35:53 +0700

    bb

--- src/layouts/vi.js
+++ src/layouts/vi.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { addLocaleData } from 'react-intl';
 
 import vi from 'react-intl/locale-data/vi';
-import 'intl/locale-data/jsonp/en';
+import 'intl/locale-data/jsonp/vi';
 import Layout from '.';
 
 addLocaleData(vi);
