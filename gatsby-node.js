7c44aeb579b73a572163d8747a8ad08e9e8c0016
nhathuy <nhathuy97ars@gmail.com>
2022-12-05 14:51:29 +0700

    bb

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,12 +2,12 @@ exports.createPages = async ({ graphql, actions }) => {
 	const { createRedirect } = actions;
 		
 	createRedirect({
-    fromPath: `/about`,
+    fromPath: `https://kkmaster.gatsbyjs.io/about/`,
     toPath: `https://kkmaster.gatsbyjs.io/aboutvn/`,
     // toPath: `https://shopeefood.vn/da-nang/nuoc-ep-detox-151-nuoc-ep-detox-nguyen-hoang.g3frat?shareChannel=zalo_message&smtt=0.0.12`,
     conditions: { 
         language: `en` 
-      } 
+    } 
 
   });
 	// createRedirect({
