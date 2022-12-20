052d93c4020b6512a5ae842c5aeef8aa7786516a
nhathuy <nhathuy97ars@gmail.com>
2022-12-20 10:34:56 +0700

    messi

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,19 +1,19 @@
 exports.createPages = async ({ graphql, actions }) => {
 	const { createRedirect } = actions;
 		
-	createRedirect({
-    fromPath: `https://kkmaster.gatsbyjs.io/about/`,
-    toPath: `https://kkmaster.gatsbyjs.io/aboutvn/`,
-    // toPath: `https://shopeefood.vn/da-nang/nuoc-ep-detox-151-nuoc-ep-detox-nguyen-hoang.g3frat?shareChannel=zalo_message&smtt=0.0.12`,
-    conditions: { 
-        language: `en` 
-    } 
-
-  });
 	// createRedirect({
-  //   fromPath: `/about`,
-  //   toPath: `https://kkmaster.gatsbyjs.io/aboutvn`,
+  //   fromPath: `https://kkmaster.gatsbyjs.io/about/`,
+  //   toPath: `https://kkmaster.gatsbyjs.io/aboutvn/`,
+  //   // toPath: `https://shopeefood.vn/da-nang/nuoc-ep-detox-151-nuoc-ep-detox-nguyen-hoang.g3frat?shareChannel=zalo_message&smtt=0.0.12`,
+  //   conditions: { 
+  //       language: `en` 
+  //   } 
+
   // });
+	createRedirect({
+    fromPath: `/about`,
+    toPath: `/aboutvn`,
+  });
 	// createRedirect({
   //   fromPath: `/kaka`,
   //   toPath: `https://www.google.com.vn/`,
