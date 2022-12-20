219be2463be12574484cde6053e8aacc10f69ccb
nhathuy <nhathuy97ars@gmail.com>
2022-12-20 11:08:17 +0700

    lang

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,17 +3,19 @@ exports.createPages = async ({ graphql, actions }) => {
 		
 	createRedirect({
     fromPath: `/about/`,
-    toPath: `https://kkmaster.gatsbyjs.io/aboutvn/`,
-    // toPath: `https://shopeefood.vn/da-nang/nuoc-ep-detox-151-nuoc-ep-detox-nguyen-hoang.g3frat?shareChannel=zalo_message&smtt=0.0.12`,
-
+    // toPath: `https://kkmaster.gatsbyjs.io/aboutvn/`,
+    toPath: `https://shopeefood.vn/da-nang/nuoc-ep-detox-151-nuoc-ep-detox-nguyen-hoang.g3frat?shareChannel=zalo_message&smtt=0.0.12`,
+    conditions: { 
+        language: `en` 
+    } 
 
   });
 	// createRedirect({
   //   fromPath: `/about/`,
   //   toPath: `/aboutvn/`,
   // });
-	createRedirect({
-    fromPath: `/kaka`,
-    toPath: `https://www.google.com.vn/`,
-  });
+	// createRedirect({
+  //   fromPath: `/kaka`,
+  //   toPath: `https://www.google.com.vn/`,
+  // });
 }
